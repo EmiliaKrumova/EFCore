35cca03c6f5a0c05409d232f577c1c882c813451
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2024-07-27 03:03:09 +0300

    added Json exercises

---
+++ 07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+        public static void Main()
+        {
+
+        }
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            return "";
+        }
+
+        private static void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+            mapper = config.CreateMapper();
+        }
+    }
+}
